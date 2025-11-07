38ce24c785fc09e996ff3a4e4e8a59458cd17c1b
Afonso Lamelas <alamelas@ua.pt>
2025-11-07 12:21:03 +0000

    The returned value now is not necessarely the acceptance rate but some quantity to weight the expected squared jump distanc

--- src/mcmc_chain.jl
+++ src/mcmc_chain.jl
@@ -6,20 +6,20 @@ function mcmc_chain(mcmc_kernel::AbstractMCMCKernel{Val{true}},target,x,state,n_
   samples = Vector{Vector{T}}(undef,n_samples)
   lps = Vector{typeof(ref_lp)}(undef,n_samples)
   gradlps = Vector{Vector{T}}(undef,n_samples)
-  α = Vector{T}(undef,n_samples-1)
+  γ = Vector{T}(undef,n_samples-1)
 
   samples[1] = x
   lps[1] = ref_lp
   gradlps[1] = ref_grad
 
   for i in 1:n_samples-1
-    samples[i+1],lps[i+1],gradlps[i+1],acc,α[i],state =
+    samples[i+1],lps[i+1],gradlps[i+1],acc,γ[i],state =
       mcmc_kernel(target,samples[i],lps[i],gradlps[i],state)
     n_accepts += acc
   end
 
 
-  return (;n_accepts,samples,lps,state,α)
+  return (;n_accepts,samples,lps,state,γ)
 end
 
 function mcmc_chain(mcmc_kernel::AbstractMCMCKernel{Val{false}},target,x,state,n_samples::Int)
@@ -29,16 +29,16 @@ function mcmc_chain(mcmc_kernel::AbstractMCMCKernel{Val{false}},target,x,state,n
   T = eltype(x)
   samples = Vector{Vector{T}}(undef,n_samples)
   lps = Vector{typeof(ref_lp)}(undef,n_samples)
-  α = Vector{T}(undef,n_samples-1)
+  γ = Vector{T}(undef,n_samples-1)
 
   samples[1] = x
   lps[1] = ref_lp
 
   for i in 1:n_samples-1
-    samples[i+1],lps[i+1],acc,α[i],state = mcmc_kernel(target,samples[i],lps[i],state)
+    samples[i+1],lps[i+1],acc,γ[i],state = mcmc_kernel(target,samples[i],lps[i],state)
     n_accepts += acc
   end
 
-  return (;n_accepts,samples,lps,state,α)
+  return (;n_accepts,samples,lps,state,γ)
 end
 

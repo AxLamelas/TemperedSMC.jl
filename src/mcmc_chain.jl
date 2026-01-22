4d79e42163020a9077d7734ba26cf307fd9df811
Afonsso Lamelas <alamelas@ua.pt>
2026-01-22 23:11:17 +0000

    Changed mcmc_chain progress to a callback

--- src/mcmc_chain.jl
+++ src/mcmc_chain.jl
@@ -1,4 +1,23 @@
-function mcmc_chain(mcmc_kernel::AbstractMCMCKernel{Val{true}},target,x,state,n_samples::Int;show_progress=false)
+struct MCMCProgressCallback{P <: ProgressMeter.Progress}
+  prog::P
+end
+
+function MCMCProgressCallback(n::Int)
+  return MCMCProgressCallback(Progress(n,desc="Sampling: ",showspeed=true))
+end
+
+function (cb::MCMCProgressCallback)(info)
+  ProgressMeter.next!(cb.prog,
+                      showvalues=[
+                      ("log density",info.log_density),
+                      ("accepted?",info.accepted)])
+  nothing
+end
+
+struct NoCallback end
+(cb::NoCallback)(args...) = nothing
+
+function mcmc_chain(mcmc_kernel::AbstractMCMCKernel{Val{true}},target,x,state,n_samples::Int;callback=NoCallback())
   n_accepts = 0
 
   ref_lp, ref_grad = LD.logdensity_and_gradient(target,x)
@@ -12,17 +31,26 @@ function mcmc_chain(mcmc_kernel::AbstractMCMCKernel{Val{true}},target,x,state,n_
   lps[1] = ref_lp
   gradlps[1] = ref_grad
 
-  @showprogress showspeed=true enabled=show_progress for i in 1:n_samples-1
+  for i in 1:n_samples-1
     samples[i+1],lps[i+1],gradlps[i+1],acc,γ[i],state =
       mcmc_kernel(target,samples[i],lps[i],gradlps[i],state)
     n_accepts += acc
+    callback(
+      (;
+        sample = samples[i+1],
+        log_density=lps[i+1],
+        grad_log_density=gradlps[i+1],
+        accepted=acc,
+        γ=γ[i],
+        state)
+    )
   end
 
 
-  return (;n_accepts,samples,lps,state,γ)
+  return (;n_accepts,samples,lps,gradlps,state,γ)
 end
 
-function mcmc_chain(mcmc_kernel::AbstractMCMCKernel{Val{false}},target,x,state,n_samples::Int;show_progress=false)
+function mcmc_chain(mcmc_kernel::AbstractMCMCKernel{Val{false}},target,x,state,n_samples::Int;callback=NoCallback())
   n_accepts = 0
 
   ref_lp = LD.logdensity(target,x)
@@ -34,9 +62,17 @@ function mcmc_chain(mcmc_kernel::AbstractMCMCKernel{Val{false}},target,x,state,n
   samples[1] = x
   lps[1] = ref_lp
 
-  @showprogress showspeed=true enabled=show_progress for i in 1:n_samples-1
+  for i in 1:n_samples-1
     samples[i+1],lps[i+1],acc,γ[i],state = mcmc_kernel(target,samples[i],lps[i],state)
     n_accepts += acc
+    callback(
+      (;
+        sample = samples[i+1],
+        log_density=lps[i+1],
+        accepted=acc,
+        γ=γ[i],
+        state)
+    )
   end
 
   return (;n_accepts,samples,lps,state,γ)
